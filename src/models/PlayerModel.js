e2e8f52cd70c396264cf5b5d0166119c830fa1f4
Laoujin <woutervs@hotmail.com>
2016-03-26 00:05:53 +0100

    moved frenoy link creation to a function

--- src/models/PlayerModel.js
+++ src/models/PlayerModel.js
@@ -14,3 +14,11 @@ export default class PlayerModel {
     return competition === 'Vttl' ? this.vttl : this.sporta;
   }
 }
+
+export function createFrenoyLink(comp) {
+  if (comp.competition === 'Vttl') {
+    return 'http://competitie.vttl.be/index.php?menu=6&result=1&sel=' + comp.frenoyLink;
+  } else {
+    return 'http://tafeltennis.sporcrea.be/competitie/index.php?menu=6&result=1&sel=' + comp.frenoyLink;
+  }
+}
