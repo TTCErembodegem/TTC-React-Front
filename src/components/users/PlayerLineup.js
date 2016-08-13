2094d522818d870c1188d2c85b11ce1561fe9507
Laoujin <woutervs@hotmail.com>
2016-08-13 02:11:14 +0200

    Profile PlayerLineup: filter on competition

--- src/components/users/PlayerLineup.js
+++ src/components/users/PlayerLineup.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { contextTypes } from '../../utils/decorators/withContext.js';
 import _ from 'lodash';
 import moment from 'moment';
+import cn from 'classnames';
 
 import { displayFormat } from '../controls/Telephone.js';
 import { selectPlayer } from '../../actions/matchActions.js';
@@ -24,6 +25,7 @@ export default class PlayerLinup extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {filter: null};
   }
 
   _onChangePlaying(match, status) {
@@ -49,56 +51,79 @@ export default class PlayerLinup extends Component {
   render() {
     const t = this.context.t;
 
-    const teams = this.props.user.getTeams();
+    const userTeams = this.props.user.getTeams();
+    var teams = userTeams;
+    if (this.state.filter) {
+      teams = teams.filter(x => x.competition === this.state.filter);
+    }
+
     const allMatchesToCome = teams.map(team => team.getMatches().toArray());
     const matches = _.uniqBy(_.flatten(allMatchesToCome), value => value.date.format('YYYYMMDD'))
       .filter(match => moment().isBefore(match.date))
       .sort((a, b) => a.date - b.date);
 
-    return (
-      <Table condensed hover>
-        <thead>
-          <tr>
-            <th className="hidden-xs">{t('common.frenoy')}</th>
-            <th className="hidden-xs">{t('common.date')}</th>
-            <th>{t('teamCalendar.match')}</th>
-            <th>{t('profile.play.tableTitle')}</th>
-          </tr>
-        </thead>
-        <tbody>
-        {matches.map(match => {
-          const getOnChangePlaying = status => this._onChangePlaying.bind(this, match, status);
-          const buttons = (
-            <ButtonToolbar>
-              <Button style={{marginBottom: 5, width: 90}} bsStyle="success" onClick={getOnChangePlaying('Play')}>{t('profile.play.canPlay')}</Button>
-              <Button style={{marginBottom: 5, width: 90}} bsStyle="danger" onClick={getOnChangePlaying('NotPlay')}>{t('profile.play.canNotPlay')}</Button>
-              <Button style={{marginBottom: 5, width: 90}} bsStyle="info" onClick={getOnChangePlaying('Maybe')}>{t('profile.play.canMaybe')}</Button>
-              <Button style={{width: 90}} onClick={getOnChangePlaying('DontKnow')}>{t('profile.play.canDontKnow')}</Button>
-            </ButtonToolbar>
-          );
-          const currentStatus = match.plays(this.props.user.playerId);
+    const allText = t('players.all');
+    const activeFilter = this.state.filter || allText;
 
-          const us = match.getTeam().renderOwnTeamTitle();
-          const them = match.renderOpponentTitle();
-          const separator = <Icon fa="fa fa-arrows-h" />;
-          return (
-            <tr key={match.id} className={this._getPlayingStatusRowClass(currentStatus)}>
-              <td className="hidden-xs">{match.frenoyMatchId}</td>
-              <td className="hidden-xs">{t('match.date', match.getDisplayDate())}</td>
-              <td>
-                <span className="visible-xs">
-                  {t('match.date', match.getDisplayDate())}
-                  <br />
-                </span>
-                <MatchVs match={match} />
-              </td>
-              <td style={{width: '1%'}} className="visible-xs">{buttons}</td>
-              <td className="hidden-xs">{buttons}</td>
+    return (
+      <div>
+        {userTeams.length > 1 ? (
+          <div className="btn-group" style={{padding: 5}}>
+            {[allText, 'Vttl', 'Sporta'].map(button => (
+              <button
+                className={cn('btn', button === activeFilter ? 'btn-info' : 'btn-default')}
+                key={button}
+                onClick={() => this.setState({filter: button === allText ? null : button})}>
+                {button}
+              </button>
+            ))}
+          </div>
+        ) : null}
+
+        <Table condensed hover>
+          <thead>
+            <tr>
+              <th className="hidden-xs">{t('common.frenoy')}</th>
+              <th className="hidden-xs">{t('common.date')}</th>
+              <th>{t('teamCalendar.match')}</th>
+              <th>{t('profile.play.tableTitle')}</th>
             </tr>
-          );
-        })}
-        </tbody>
-      </Table>
+          </thead>
+          <tbody>
+          {matches.map(match => {
+            const getOnChangePlaying = status => this._onChangePlaying.bind(this, match, status);
+            const buttons = (
+              <ButtonToolbar>
+                <Button style={{marginBottom: 5, width: 90}} bsStyle="success" onClick={getOnChangePlaying('Play')}>{t('profile.play.canPlay')}</Button>
+                <Button style={{marginBottom: 5, width: 90}} bsStyle="danger" onClick={getOnChangePlaying('NotPlay')}>{t('profile.play.canNotPlay')}</Button>
+                <Button style={{marginBottom: 5, width: 90}} bsStyle="info" onClick={getOnChangePlaying('Maybe')}>{t('profile.play.canMaybe')}</Button>
+                <Button style={{width: 90}} onClick={getOnChangePlaying('DontKnow')}>{t('profile.play.canDontKnow')}</Button>
+              </ButtonToolbar>
+            );
+            const currentStatus = match.plays(this.props.user.playerId);
+
+            const us = match.getTeam().renderOwnTeamTitle();
+            const them = match.renderOpponentTitle();
+            const separator = <Icon fa="fa fa-arrows-h" />;
+            return (
+              <tr key={match.id} className={this._getPlayingStatusRowClass(currentStatus)}>
+                <td className="hidden-xs">{match.frenoyMatchId}</td>
+                <td className="hidden-xs">{t('match.date', match.getDisplayDate())}</td>
+                <td>
+                  <span className="visible-xs">
+                    {t('match.date', match.getDisplayDate())}
+                    <br />
+                  </span>
+                  <MatchVs match={match} />
+                </td>
+                <td style={{width: '1%'}} className="visible-xs">{buttons}</td>
+                <td className="hidden-xs">{buttons}</td>
+              </tr>
+            );
+          })}
+          </tbody>
+        </Table>
+      </div>
     );
   }
 }
