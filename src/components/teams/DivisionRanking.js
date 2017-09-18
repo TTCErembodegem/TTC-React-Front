957723412edc0a89f4c6383f9c9f94c33bb19981
Laoujin <woutervs@hotmail.com>
2017-09-18 23:51:30 +0200

    team divisionRanking: isForfait

--- src/components/teams/DivisionRanking.js
+++ src/components/teams/DivisionRanking.js
@@ -4,34 +4,37 @@ import cn from 'classnames';
 import { OwnClubId } from '../../models/ClubModel.js';
 import storeUtil from '../../storeUtil.js';
 
-const DivisionRanking = ({team, t}) => (
-  <Table condensed hover>
-    <thead>
-      <tr>
-        <th>{t('teamCalendar.position')}</th>
-        <th>{t('teamCalendar.name')}</th>
-        <th className="hidden-xs">{t('teamCalendar.matchesWon')}</th>
-        <th className="hidden-xs">{t('teamCalendar.matchesLost')}</th>
-        <th className="hidden-xs">{t('teamCalendar.matchesDraw')}</th>
-        <th>{t('teamCalendar.points')}</th>
-      </tr>
-    </thead>
-    <tbody>
-      {team.ranking.map(teamRanking => (
-        <tr
-          className={cn({'match-won accentuate': teamRanking.clubId === OwnClubId})}
-          key={teamRanking.clubId + teamRanking.teamCode}
-        >
-          <td>{teamRanking.position}</td>
-          <td>{storeUtil.getClub(teamRanking.clubId).name + ' ' + teamRanking.teamCode}</td>
-          <td className="hidden-xs">{teamRanking.gamesWon}</td>
-          <td className="hidden-xs">{teamRanking.gamesLost}</td>
-          <td className="hidden-xs">{teamRanking.gamesDraw}</td>
-          <td>{teamRanking.points}</td>
+
+const DivisionRanking = ({team, t}) => {
+  return (
+    <Table condensed hover>
+      <thead>
+        <tr>
+          <th>{t('teamCalendar.position')}</th>
+          <th>{t('teamCalendar.name')}</th>
+          <th className="hidden-xs">{t('teamCalendar.matchesWon')}</th>
+          <th className="hidden-xs">{t('teamCalendar.matchesLost')}</th>
+          <th className="hidden-xs">{t('teamCalendar.matchesDraw')}</th>
+          <th>{t('teamCalendar.points')}</th>
         </tr>
-      ))}
-    </tbody>
-  </Table>
-);
+      </thead>
+      <tbody>
+        {team.ranking.map(teamRanking => (
+          <tr
+            className={cn({'match-won accentuate': teamRanking.clubId === OwnClubId, 'match-lost': teamRanking.isForfait})}
+            key={teamRanking.clubId + teamRanking.teamCode}
+          >
+            <td>{teamRanking.position}</td>
+            <td>{storeUtil.getClub(teamRanking.clubId).name + ' ' + teamRanking.teamCode}</td>
+            <td className="hidden-xs">{teamRanking.gamesWon}</td>
+            <td className="hidden-xs">{teamRanking.gamesLost}</td>
+            <td className="hidden-xs">{teamRanking.gamesDraw}</td>
+            <td>{teamRanking.points}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
+};
 
 export default DivisionRanking;
