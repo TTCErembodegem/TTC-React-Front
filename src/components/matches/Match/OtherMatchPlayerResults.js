d3b51142887db697566a5cc074958e24768a2177
Laoujin <woutervs@hotmail.com>
2017-10-04 02:43:57 +0200

    responsive

--- src/components/matches/Match/OtherMatchPlayerResults.js
+++ src/components/matches/Match/OtherMatchPlayerResults.js
@@ -12,12 +12,12 @@ export class OtherMatchPlayerResults extends Component {
     const match = this.props.match;
     return (
       <div className="row">
-        <div className="col-md-6">
+        <div className="col-sm-6">
           <h4 style={{marginTop: 5}}>{match.getClub('home').name} {match.home.teamCode}</h4>
           {match.getOwnPlayers().map(ply => <OpponentPlayer ply={ply} key={ply.position} t={this.context.t} competition={match.competition} />)}
         </div>
 
-        <div className="col-md-6">
+        <div className="col-sm-6">
           <h4>{match.getClub('away').name} {match.away.teamCode}</h4>
           {match.getTheirPlayers().map(ply => <OpponentPlayer ply={ply} key={ply.position} t={this.context.t} competition={match.competition} />)}
         </div>
