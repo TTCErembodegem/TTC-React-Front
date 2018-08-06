d5d14a11b1b89a4ed8150e5a79a17e33ee556b01
Laoujin <woutervs@hotmail.com>
2018-08-06 22:15:14 +0200

    more itenium

--- src/components/skeleton/Footer/Footer.js
+++ src/components/skeleton/Footer/Footer.js
@@ -17,21 +17,39 @@ export default class Footer extends Component {
 
   render() {
     return (
-      <div className="Footer">
-        <div className="Footer-container">
-          <div className="col-xs-2 col-sm-1"><Icon fa="fa fa-map-marker" /></div>
-          <div className="col-xs-13 col-sm-5 Footer-text">{this.props.params.location}</div>
-          <div className="col-xs-2 col-sm-1"><Icon fa="fa fa-calendar" /></div>
-          <div className="col-xs-13 col-sm-5 Footer-text">{this.props.params.trainingDays}</div>
-          <div className="col-xs-2 col-sm-1">&nbsp;</div>
-          <div className="col-xs-13 col-sm-5 Footer-text">{this.props.params.competitionDays}</div>
-          <div className="col-xs-2 col-sm-1" ><Icon fa="fa fa-envelope-o" /></div>
-          <div className="col-xs-13 col-sm-5 Footer-text">
-            <OwnEmail className="Footer-link" />
-          </div>
-        </div>
+      <div>
+        <MadeBy />
 
-        <CookieNotice />
+        <div className="Footer">
+          <div className="Footer-container">
+            <div className="col-xs-2 col-sm-1"><Icon fa="fa fa-map-marker" /></div>
+            <div className="col-xs-13 col-sm-5 Footer-text">{this.props.params.location}</div>
+            <div className="col-xs-2 col-sm-1"><Icon fa="fa fa-calendar" /></div>
+            <div className="col-xs-13 col-sm-5 Footer-text">{this.props.params.trainingDays}</div>
+            <div className="col-xs-2 col-sm-1">&nbsp;</div>
+            <div className="col-xs-13 col-sm-5 Footer-text">{this.props.params.competitionDays}</div>
+            <div className="col-xs-2 col-sm-1" ><Icon fa="fa fa-envelope-o" /></div>
+            <div className="col-xs-13 col-sm-5 Footer-text">
+              <OwnEmail className="Footer-link" />
+            </div>
+          </div>
+          <CookieNotice />
+        </div>
+      </div>
+    );
+  }
+}
+
+class MadeBy extends Component {
+  render() {
+    const style = {
+      float: 'right',
+      marginRight: 20
+    };
+    return (
+      <div>
+        <div style={style}>by <a href="http://itenium.be" target="_blank">itenium</a></div>
+        <div style={{clear: 'both'}} />
       </div>
     );
   }
